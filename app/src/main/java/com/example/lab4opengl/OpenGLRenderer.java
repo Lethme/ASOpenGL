837c3bf615fec9813e143525ffc09d4b2eea10e1
PeshkaAbramovoi <lethme@ya.ru>
2020-11-06 18:08:51 +0400

    Bear

--- app/src/main/java/com/example/lab4opengl/OpenGLRenderer.java
+++ app/src/main/java/com/example/lab4opengl/OpenGLRenderer.java
@@ -10,14 +10,209 @@ import javax.microedition.khronos.opengles.GL10;
 public class OpenGLRenderer implements GLSurfaceView.Renderer {
     private FloatBuffer vertexBuffer;
     private float vertices[];
-    private float angle = 0.0f;
+    private final int LightBrownTrianglesCount = 22;
+    private final int DarkBrownTrianglesCount = 20;
+    private final int WhiteTrianglesCount = 2;
+    private final int BlackTrianglesCount = 2;
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig eglConfig) {
         //Устанавливаем цвет очистки("заливки") экрана в черный
         gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         //Устанавливаем X, Y, Z координаты точек вершин и записываем их в массив vertices
-        vertices = new float[]{-0.5f, -0.5f, 0.0f, -0.5f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.5f, 0.5f, 0.0f};
+        vertices = new float[]{
+                /* Dark brown triangles */
+                0.0f, 0.0f,
+                -0.1f, -0.8f,
+                0.1f, -0.8f,
+
+                -0.6f, 0.1f,
+                -0.35f, -0.9f,
+                -0.1f, -0.8f,
+
+                0.6f, 0.1f,
+                0.35f, -0.9f,
+                0.1f, -0.8f,
+
+                -0.35f, -0.9f,
+                -0.45f, -0.95f,
+                -0.08f, -0.95f,
+
+                0.35f, -0.9f,
+                0.45f, -0.95f,
+                0.08f, -0.95f,
+
+                -0.475f, -0.4f,
+                -0.7f, -0.95f,
+                -0.75f, -0.92f,
+
+                0.475f, -0.4f,
+                0.7f, -0.95f,
+                0.75f, -0.92f,
+
+                0.0f, 0.0f,
+                -0.4f, 0.6f,
+                -0.35f, 0.3f,
+
+                0.0f, 0.0f,
+                0.4f, 0.6f,
+                0.35f, 0.3f,
+
+                0.0f, 0.15f,
+                -0.1f, 0.2f,
+                0.1f, 0.2f,
+
+                0.0f, 0.2f,
+                -0.1f, 0.2f,
+                -0.08f, 0.23f,
+
+                0.0f, 0.2f,
+                0.1f, 0.2f,
+                0.08f, 0.23f,
+
+                0.0f, 0.2f,
+                -0.08f, 0.23f,
+                0.0f, 0.25f,
+
+                0.0f, 0.2f,
+                0.08f, 0.23f,
+                0.0f, 0.25f,
+
+                0.0f, 0.6f,
+                -0.3f, 0.7f,
+                0.3f, 0.7f,
+
+                -0.3f, 0.7f,
+                0.3f, 0.7f,
+                0.0f, 0.8f,
+
+                -0.3f, 0.7f,
+                -0.36f, 0.74f,
+                -0.4f, 0.7f,
+
+                0.3f, 0.7f,
+                0.36f, 0.74f,
+                0.4f, 0.7f,
+
+                -0.3f, 0.7f,
+                -0.4f, 0.7f,
+                -0.38f, 0.62f,
+
+                0.3f, 0.7f,
+                0.4f, 0.7f,
+                0.38f, 0.62f,
+                /* Dark brown triangles */
+
+                /* Light brown triangles */
+                -0.1f, -0.8f,
+                -0.35f, -0.9f,
+                -0.08f, -0.95f,
+
+                0.1f, -0.8f,
+                0.35f, -0.9f,
+                0.08f, -0.95f,
+
+                -0.1f, -0.8f,
+                0.1f, -0.8f,
+                -0.08f, -0.95f,
+
+                0.1f, -0.8f,
+                -0.08f, -0.95f,
+                0.08f, -0.95f,
+
+                -0.475f, -0.4f,
+                -0.7f, -0.95f,
+                -0.35f, -0.9f,
+
+                0.475f, -0.4f,
+                0.7f, -0.95f,
+                0.35f, -0.9f,
+
+                -0.45f, -0.95f,
+                -0.7f, -0.95f,
+                -0.35f, -0.9f,
+
+                0.45f, -0.95f,
+                0.7f, -0.95f,
+                0.35f, -0.9f,
+
+                -0.75f, -0.92f,
+                -0.85f, -0.8f,
+                -0.65f, -0.84f,
+
+                0.75f, -0.92f,
+                0.85f, -0.8f,
+                0.65f, -0.84f,
+
+                0.0f, 0.0f,
+                -0.4f, 0.6f,
+                0.0f, 0.6f,
+
+                0.0f, 0.0f,
+                0.4f, 0.6f,
+                0.0f, 0.6f,
+
+                -0.4f, 0.6f,
+                0.0f, 0.6f,
+                -0.3f, 0.7f,
+
+                0.4f, 0.6f,
+                0.0f, 0.6f,
+                0.3f, 0.7f,
+
+                0.0f, 0.0f,
+                -0.1f, -0.8f,
+                -0.6f, 0.1f,
+
+                0.0f, 0.0f,
+                0.1f, -0.8f,
+                0.6f, 0.1f,
+
+                -0.35f, 0.3f,
+                -0.6f, 0.1f,
+                0.0f, 0.0f,
+
+                0.35f, 0.3f,
+                0.6f, 0.1f,
+                0.0f, 0.0f,
+
+                -0.36f, 0.74f,
+                -0.3f, 0.7f,
+                -0.15f, 0.75f,
+
+                0.36f, 0.74f,
+                0.3f, 0.7f,
+                0.15f, 0.75f,
+
+                -0.36f, 0.74f,
+                -0.27f, 0.78f,
+                -0.15f, 0.75f,
+
+                0.36f, 0.74f,
+                0.27f, 0.78f,
+                0.15f, 0.75f,
+                /* Light brown triangles */
+
+                /* White triangles */
+                -0.15f, 0.5f,
+                -0.25f, 0.4f,
+                -0.05f, 0.4f,
+
+                0.15f, 0.5f,
+                0.25f, 0.4f,
+                0.05f, 0.4f,
+                /* White triangles */
+
+                /* Black triangles */
+                -0.15f, 0.5f,
+                -0.18f, 0.47f,
+                -0.12f, 0.47f,
+
+                0.22f, 0.44f,
+                0.25f, 0.4f,
+                0.18f, 0.4f,
+                /* Black triangles */
+        };
         //Создаем буфер вершин, содержащий наш массив
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
         byteBuffer.order(ByteOrder.nativeOrder());
@@ -41,16 +236,38 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         //Включаем поддержку массивов вершин
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
         //Устанавливаем цвет серый
-        gl.glColor4f(0.5f, 0.5f, 0.5f, 1.0f);
         //Указываем размер, тип и буфер с точками
         //Третий параметр, который равен нулю, нас пока не интересует
-        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+        gl.glVertexPointer(2, GL10.GL_FLOAT, 0, vertexBuffer);
+
+        gl.glColor4f(0.3f, 0.22f, 0.16f, 1.0f);
+        for (int i = 0, first = 60; i < LightBrownTrianglesCount; i++, first += 3)
+        {
+            gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, first, 3);
+        }
+
+        gl.glColor4f(0.2f, 0.15f, 0.12f, 1.0f);
         //Рисуем по принципу GL_TRIANGLE_STRIP
-        gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+        for (int i = 0, first = 0; i < DarkBrownTrianglesCount; i++, first += 3)
+        {
+            gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, first, 3);
+        }
+
+        gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        //Рисуем по принципу GL_TRIANGLE_STRIP
+        for (int i = 0, first = 126; i < WhiteTrianglesCount; i++, first += 3)
+        {
+            gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, first, 3);
+        }
+
+        gl.glColor4f(0f, 0f, 0f, 1.0f);
+        //Рисуем по принципу GL_TRIANGLE_STRIP
+        for (int i = 0, first = 132; i < WhiteTrianglesCount; i++, first += 3)
+        {
+            gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, first, 3);
+        }
+
         //Сбрасываем матрицу(об этом ниже)
         gl.glLoadIdentity();
-        //Вращаем текущую матрицу(наш объект, если сильно утрировать)
-        gl.glRotatef(angle, 1.0f, 1.0f, 1.0f);
-        angle++;
     }
 }
