5ea95d66c44108190a05c28fdf27c4b5d16a7b4f
PeshkaAbramovoi <lethme@ya.ru>
2020-11-11 11:54:49 +0400

    Test

--- app/src/main/java/com/example/lab4opengl/Graphics/Pictures/Picture.java
+++ app/src/main/java/com/example/lab4opengl/Graphics/Pictures/Picture.java
@@ -1,11 +1,13 @@
 package com.example.lab4opengl.Graphics.Pictures;
 
+import com.example.lab4opengl.Graphics.Figures.Ellipse;
 import com.example.lab4opengl.Graphics.Figures.Figure;
+import com.example.lab4opengl.Graphics.Figures.Line;
+import com.example.lab4opengl.Graphics.Figures.Rectangle;
 import com.example.lab4opengl.Graphics.Figures.Triangle;
 import com.example.lab4opengl.Graphics.Graphics;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Picture implements Pictures {
     protected ArrayList<Figure> figures;
@@ -24,16 +26,6 @@ public class Picture implements Pictures {
         }
     }
 
-    public <T extends Figure> ArrayList<T> getFigures() {
-        ArrayList<T> list = new ArrayList<T>();
-        for (Figure figure : figures) {
-            if (figure.getClass() == Triangle.class) {
-                list.add((T)figure);
-            }
-        }
-        return list;
-    }
-
     public void Draw() {
         Graphics.Clear();
         for (Figure figure : this.figures) {
@@ -47,5 +39,49 @@ public class Picture implements Pictures {
     }
     public static Picture Create(Figure... figures) {
         return new Picture(figures);
+    }
+
+    @Override
+    public ArrayList<Triangle> getTriangles() {
+        ArrayList<Triangle> list = new ArrayList<>();
+        for (Figure figure : figures) {
+            if (figure.getClass() == Triangle.class) {
+                list.add((Triangle) figure);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public ArrayList<Rectangle> getRectangles() {
+        ArrayList<Rectangle> list = new ArrayList<>();
+        for (Figure figure : figures) {
+            if (figure.getClass() == Rectangle.class) {
+                list.add((Rectangle) figure);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public ArrayList<Ellipse> getEllipses() {
+        ArrayList<Ellipse> list = new ArrayList<>();
+        for (Figure figure : figures) {
+            if (figure.getClass() == Ellipse.class) {
+                list.add((Ellipse) figure);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public ArrayList<Line> getLines() {
+        ArrayList<Line> list = new ArrayList<>();
+        for (Figure figure : figures) {
+            if (figure.getClass() == Line.class) {
+                list.add((Line) figure);
+            }
+        }
+        return list;
     }
 }
--- app/src/main/java/com/example/lab4opengl/Graphics/Pictures/Pictures.java
+++ app/src/main/java/com/example/lab4opengl/Graphics/Pictures/Pictures.java
@@ -1,9 +1,15 @@
 package com.example.lab4opengl.Graphics.Pictures;
 
-import com.example.lab4opengl.Graphics.Figures.Figure;
+import com.example.lab4opengl.Graphics.Figures.Ellipse;
+import com.example.lab4opengl.Graphics.Figures.Line;
+import com.example.lab4opengl.Graphics.Figures.Rectangle;
+import com.example.lab4opengl.Graphics.Figures.Triangle;
 
 import java.util.ArrayList;
 
 public interface Pictures {
-    public <T extends Figure> ArrayList<T> getFigures();
+    public ArrayList<Triangle> getTriangles();
+    public ArrayList<Rectangle> getRectangles();
+    public ArrayList<Ellipse> getEllipses();
+    public ArrayList<Line> getLines();
 }
